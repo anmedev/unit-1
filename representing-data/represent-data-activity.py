8500391929c3846f673e35698a24f5c98479dbeb
anmedev <aam2ds@virginia.edu>
2023-01-24 19:44:54 -0500

    add file and solutions for activity

---
+++ representing-data/represent-data-activity.py
@@ -0,0 +1,5 @@
+# Question 1: Adapt to a List
+highest_scores = [1099, 1402, 1398, 989, 972, 1092, 1115, 1108, 1100, 1142, 1136, 1025]
+
+# Question 2: Adapt to a Dictionary
+patient_summary = patient_summary = {"name": "Nelly", "date": "06/01/2015", "mailing_address": "4829 Hazelwood Ave., Apt. 712, Des Moines, IA 50309", "email": "nelly.the.jelly", "policy_number": 4001337200, "rbc": 5.78, "hematocrit": 17.9, "test_summary": "satisfactory"}

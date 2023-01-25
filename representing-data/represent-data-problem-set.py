e8bbad9f987807dbc556c6850451a2dbc2d715d1
anmedev <aam2ds@virginia.edu>
2023-01-25 17:43:47 -0500

    add solutions for remaining exercises

--- representing-data/represent-data-problem-set.py
+++ representing-data/represent-data-problem-set.py
@@ -1,11 +1,36 @@
-# Representing Data - dZMTTi
+# Representing Data - ID: dZMTTi
 def get_friend_data():
   animal_sightings = {"date": "2010-06-01", "location": "park", "crow_count": 3, "dog_count": 10, "cat_count" : 0, "pigeon_count" : 31}
   return animal_sightings
 print(get_friend_data())
 
-# Representing Data - KkABtU
+# Representing Data - ID: KkABtU
 def sort_contestants_age_desc():
   names = ["Dane", "Neo", "Amara", "Mae", "Jules", "Brandi"]
   return names
 print(sort_contestants_age_desc())
+
+def get_my_preferences():
+  data = {"name": "A.M.", "is_morning_person": True, "is_night_owl": False, "fav_song": None, "num_of_household_members": 4}
+  return data
+print(get_my_preferences())
+
+# Representing Data - ID: mHGBgU
+def add_preferences(preferences, category, value):
+  preferences[category] = value
+  return preferences
+print(add_preferences({"name": "A.M.", "is_morning_person": True, "is_night_owl": False, "fav_song": None, "num_of_household_members": 4}, "age", 26))
+
+# Representing Data - ID: 5LPH50
+def add_grocery(grocery_list, new_item):
+  grocery_list.append(new_item)
+  return grocery_list
+print(add_grocery(["eggs", "milk", "bread", "spinach", "broccoli"], "butter"))
+
+# Representing Data - ID: LLU2tX
+
+def copy_top_item(my_groceries, their_groceries):
+  top_item = their_groceries[0]
+  my_groceries[0] = top_item
+  return my_groceries
+print(copy_top_item(["eggs", "milk", "bread", "spinach", "broccoli", "butter"], ["chocolate", "ice cream", "bananas", "tomatoes", "chicken", "onion"]))

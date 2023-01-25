a8dd03cd3a8ab9d39394bf2f50ab91881e8af802
anmedev <aam2ds@virginia.edu>
2023-01-24 19:53:11 -0500

    addinitial solution for problem set

---
+++ representing-data/represent-data-problem-set.py
@@ -0,0 +1,4 @@
+def get_friend_data():
+  animal_sightings = {"date": "2010-06-01", "location": "park", "crow_count": 3, "dog_count": 10, "cat_count" : 0, "pigeon_count" : 31}
+  return animal_sightings
+print(get_friend_data())

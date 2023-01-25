4009ca29232e2b03b59f213e82b1d93cd065b0b1
anmedev <aam2ds@virginia.edu>
2023-01-25 16:54:03 -0500

    add solution for exercise

--- representing-data/represent-data-problem-set.py
+++ representing-data/represent-data-problem-set.py
@@ -1,4 +1,11 @@
+# Representing Data - dZMTTi
 def get_friend_data():
   animal_sightings = {"date": "2010-06-01", "location": "park", "crow_count": 3, "dog_count": 10, "cat_count" : 0, "pigeon_count" : 31}
   return animal_sightings
 print(get_friend_data())
+
+# Representing Data - KkABtU
+def sort_contestants_age_desc():
+  names = ["Dane", "Neo", "Amara", "Mae", "Jules", "Brandi"]
+  return names
+print(sort_contestants_age_desc())

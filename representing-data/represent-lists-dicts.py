78926dc7724da3347a1e3e63a6bf305b532a60d4
anmedev <aam2ds@virginia.edu>
2023-01-24 19:29:04 -0500

    add solution for dictionaries exercise

--- representing-data/represent-lists-dicts.py
+++ representing-data/represent-lists-dicts.py
@@ -10,3 +10,7 @@ def dna_differences():
       final_dna.append(dna_2[i])
   print(final_dna)
 dna_differences() # ["G", "C", "G", "T", "C", "G", "C", "A", "A", "G", "G", "C", "A", "C"]
+
+# Dictionaries Exercise
+time_and_colors = {"08:00": ["red", "orange", "green"], "09:00": ["red", "orange"], "10:00": ["blue"], "11:00": ["blue", "green"], "12:00": ["blue", "gray"], "13:00": ["red", "blue"], "14:00": ["white"], "15:00": ["green", "white"], "16:00": ["white", "black"], "17:00": ["yellow", "black"], "18:00": ["white", "green", "gray", "red", "blue"]}
+color_frequency = {"red": 4, "blue": 5, "yellow": 1, "green": 4, "orange": 2, "gray": 2, "black": 2, "white": 4}

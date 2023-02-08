680959fcc1a19db63f5285914b7e925c999850a5
anmedev <aam2ds@virginia.edu>
2023-02-07 19:51:36 -0500

    add files and folders for conditionals section

---
+++ conditionals/conditionals-problem-set.py
@@ -0,0 +1,14 @@
+# Conditionals Recap
+# Assume we have the following variable declarations -- select all options that will evaluate to true:
+var1 = None
+var2 = 1
+var3 = {}
+var4 = [0, 0]
+var5 = "None"
+
+# var1 or var2
+# var5 and var2
+# var4 or var3
+# var5 or var3
+# var2 or not var5
+# not (var3 or var1)

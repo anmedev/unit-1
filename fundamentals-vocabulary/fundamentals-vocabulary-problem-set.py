6dae5f28d821c7e49a00eab6a1534d7561b09add
anmedev <aam2ds@virginia.edu>
2023-02-09 18:19:46 -0500

    add file and solutions for exercises

---
+++ fundamentals-vocabulary/fundamentals-vocabulary-problem-set.py
@@ -0,0 +1,15 @@
+# Expressions and Statements - id: e255bf2d-f703-4a22-ba13-f142584bba93
+# There is an expression on line 2. Identify the expression on line 2.
+test_num = 5
+if test_num > 10:
+  print("Bigger than ten!")
+
+# test num > 10
+
+# Literals - id: f07c693b-b2f2-4e16-a114-74cf6fcfe43a
+# Using the code snippet below, identify at least 1 literal:
+test_num = 5
+if test_num > 10:
+  print("Bigger than ten!")
+
+# "Bigger than ten!", other literals include 5 and 10

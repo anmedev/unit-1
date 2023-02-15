b0f57eb8239af462a463b9257dca42f94afa66a9
anmedev <aam2ds@virginia.edu>
2023-02-15 17:32:37 -0500

    add file and solutions for problem set

---
+++ intro-to-functions/intro-to-functions-problem-set.py
@@ -0,0 +1,84 @@
+# Problem Statements and Functions - id: 7eaadcae-9001-4d2f-a6e6-3242e28ac4f1
+# Imagine that you maintain a lot of plants. For any given plant, you need to check if it needs water, check if it needs sunlight, and a few other tasks. You're creating one function to describe all of those tasks. Which of these is the best name for this function?
+
+# Answer: maintain_plant(plant)
+
+# Problem Statements and Functions - id: ooI7dJ
+# You are writing a function named needs_water, which will return whether or not a plant needs water. What data type is this needs_water most likely going to return?
+
+# Answer: Boolean
+
+# Problem Statements and Functions - id: EPPL9w
+# You are writing a function named relocate_plant, which will take one plant and a proposed new spot for it, and move the plant there. Which one of these is the best set of parameters?
+
+# Answer: relocate_plant(plant, location)
+
+# Syntax - id: v6k6kC
+# Given this code...identify this piece of syntax: def maintain_plant(plant):
+
+# def maintain_plant(plant):
+#     if needs_water(plant):
+#         water(plant)
+#     if needs_sunlight(plant):
+#         new_location = find_available_sunny_spot(plant)
+#         relocate(plant, new_location)
+#     sing_to_plant(plant, "A Mellow Mood for Maidenhair")
+#     return plant
+
+# Answer: Function Signature
+
+# Syntax - id: 8TrnuR
+# Given this code (duplicated from above)...specifically in the line def maintain_plant(plant):, identify this piece of syntax: plant
+
+# def maintain_plant(plant):
+#     if needs_water(plant):
+#         water(plant)
+#     if needs_sunlight(plant):
+#         new_location = find_available_sunny_spot(plant)
+#         relocate(plant, new_location)
+#     sing_to_plant(plant, "A Mellow Mood for Maidenhair")
+#     return plant
+
+# Answer: Parameter
+
+# Syntax - id: owr3gL
+# Given this code (duplicated from above)...specifically in the line def maintain_plant(plant):, identify this piece of syntax: maintain_plant
+
+# def maintain_plant(plant):
+#     if needs_water(plant):
+#         water(plant)
+#     if needs_sunlight(plant):
+#         new_location = find_available_sunny_spot(plant)
+#         relocate(plant, new_location)
+#     sing_to_plant(plant, "A Mellow Mood for Maidenhair")
+#     return plant
+
+# Answer: Function name
+
+# Syntax - id: owr3gL
+# Given this code (duplicated from above)...identify this piece of syntax: relocate(plant, new_location)
+
+# def maintain_plant(plant):
+#     if needs_water(plant):
+#         water(plant)
+#     if needs_sunlight(plant):
+#         new_location = find_available_sunny_spot(plant)
+#         relocate(plant, new_location)
+#     sing_to_plant(plant, "A Mellow Mood for Maidenhair")
+#     return plant
+
+# Answer: Function call
+
+# Syntax - id: owr3gL
+# Given this code (duplicated from above)...identify this piece of syntax: "A Mellow Mood for Maidenhair"
+
+# def maintain_plant(plant):
+#     if needs_water(plant):
+#         water(plant)
+#     if needs_sunlight(plant):
+#         new_location = find_available_sunny_spot(plant)
+#         relocate(plant, new_location)
+#     sing_to_plant(plant, "A Mellow Mood for Maidenhair")
+#     return plant
+
+# Answer: Argument

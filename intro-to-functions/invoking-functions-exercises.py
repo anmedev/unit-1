820c667a8c43eaa6a9bf8ccde5b209ec16d2eb83
anmedev <aam2ds@virginia.edu>
2023-02-14 20:06:09 -0500

    add file and solutions for invoking functions exercises

---
+++ intro-to-functions/invoking-functions-exercises.py
@@ -0,0 +1,26 @@
+# Invoking Synonyms - id: ec392164-68de-4e37-a0c0-26ccd20f5cf6
+# Assume there is a function named perform_show(). 
+
+# Calling perform_show()
+# Executing perform_show()
+# Evaluating perform_show()
+
+# Function Name - id: a667287f-6322-490d-a5e3-fa32b8ef1d41
+# What is the name of the function being called?
+# performance = perform_show("Tyra's Magic Show", ["rabbit", "deck of cards"])
+
+# perform_show
+
+# Function Arguments - id: 5874f1be-0457-4d35-9f1e-f0876502eda1
+# How many arguments are being passed into this function call?
+# performance = perform_show("Tyra's Magic Show", ["rabbit", "deck of cards"])
+
+# 2
+
+# Functions and Variables - id: 346c043f-969e-426d-afd1-15aafa603e45
+# What is performance?
+# performance = perform_show("Tyra's Magic Show", ["rabbit", "deck of cards"])
+
+# A variable that holds the return value of perform_show("Tyra's Magic Show", ["rabbit", "deck of cards"])
+
+

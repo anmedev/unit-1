58cfa6da455d13b2de8c7ed22a3a962d59a71ab3
anmedev <aam2ds@virginia.edu>
2023-02-16 13:00:05 -0500

    add file and solution for exercises

---
+++ intro-to-functions/how-to-read-documentation-exercises.py
@@ -0,0 +1,8 @@
+# Reading Documentation - id: 22ca8275-4368-4940-b758-ac79e3222f24
+
+# What are some important aspects of documentation we should look out for?
+
+# Version of Documentation
+# Section Headings of docs, especially ones that describe the kind of functionality you're looking for
+# Lists of functions
+# Endpoints

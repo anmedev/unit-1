f885d217327cb1c319e97dca611f54c5d179fd8f
anmedev <aam2ds@virginia.edu>
2023-02-20 18:10:46 -0500

    add files and folder for intro to debugging lesson

---
+++ intro-to-debugging/intro-to-errors-exercises.py
@@ -0,0 +1,36 @@
+# Errors - id: bd266c76-9e23-448e-b93e-b870e894c2bf
+# What kind of error does the following code produce?
+# for i in range
+#     print("Hello")
+
+# Answer: Syntax Error
+
+# Errors - id: 59bd8c4c-9113-4e20-9bd9-f93b3a7e1f93
+# What kind of error does the following code produce?
+# def addition():
+#     num1 = 2
+#     num2 = 3
+#     print(num1+num2)
+# add_together()
+
+# Answer: Runtime Error
+
+# Errors - id: 52579603-887f-4085-8448-ef4e8f00405a
+# What kind of error does the following code produce?
+# def addition():
+#     num1 = input("Enter a number")
+#     num2 = input("Enter another number")
+#     print(num1+num2)
+# addition()
+
+# Answer: Logical Error
+
+# Errors - id: 52579603-887f-4085-8448-ef4e8f00405a
+# What kind of error does the following code produce?
+# def multiply():
+# num1 = 2
+# num2 = 2
+# print(num1*num2)
+# multiply()
+
+# Answer: Syntax Error

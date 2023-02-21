5d9a0c8e63d7b75c8564cc2b4f9bd3d4460c5269
anmedev <aam2ds@virginia.edu>
2023-02-20 19:45:54 -0500

    add file and solution for exercise

---
+++ intro-to-debugging/intro-to-debugging-exercises.py
@@ -0,0 +1,13 @@
+# Debugging Methods - id: 27fc1277-cb27-426a-aff8-0298638fb5f0
+# Select all methods that are valid ways to approach debugging.
+
+# Answer:
+# Asking "what" is happening
+# Talking to a literal rubber duck
+# Talking to a classmate
+# Writing down what you expected to happen vs. what actually happened
+# Taking a break and looking at it again in an hour
+# Using print statements to confirm the value of a variable on a given line
+# Making theories and experiments on why the bug is there and how to fix it
+# Taking notes about the bug
+# Making a table or diagram

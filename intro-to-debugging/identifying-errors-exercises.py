8c53e540de202f3b0e9b13543b24a087b5cd0713
anmedev <aam2ds@virginia.edu>
2023-02-20 19:18:34 -0500

    add file and solutions for exercises

---
+++ intro-to-debugging/identifying-errors-exercises.py
@@ -0,0 +1,27 @@
+# What is the error description? - id: 85c12282-66c8-46d9-ae20-10e73cdb6f5a
+# Given this stack trace output, what is the description of the error?
+
+#   File "main.py", line 5
+#     if apple_quantity < 1
+#                         ^
+# SyntaxError: invalid syntax
+
+# Answer: invalid syntax
+
+# What is the name of this error? - id: 2cb0d84c-a5eb-44f3-8bee-5d7e61e5e47f
+# Given this stack trace output, what is the description of the error?
+#   File "main.py", line 5
+#     if apple_quantity < 1
+#                         ^
+# SyntaxError: invalid syntax
+
+# Answer: SyntaxError
+
+# What is the location of this error? - id: 889eac81-84e0-46fe-a157-25102c56e28c
+# Given this stack trace output, what is the location of the line of code that caused the error?
+#   File "main.py", line 5
+#     if apple_quantity < 1
+#                         ^
+# SyntaxError: invalid syntax
+
+# Answer: main.py, line 5

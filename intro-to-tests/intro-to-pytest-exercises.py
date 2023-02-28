8420f8689aa03a35608cca3acc2474f26afc00c8
anmedev <aam2ds@virginia.edu>
2023-02-27 19:59:05 -0500

    add solutions for exercises

---
+++ intro-to-tests/intro-to-pytest-exercises.py
@@ -0,0 +1,24 @@
+# Intro to Pytest - id: a1NxH7
+# Rearrange these lines so they follow the logical order that a unit test is normally in.
+
+# Answer:
+# 1. Test name
+# 2. Arrange step, where setting up any variables is
+# 3. Act step, where we call the tested function
+# 4. Assert step, where we check that the result of the tested function is what we expected
+
+# Intro to Pytest - id: M9WsZl
+# Rearrange these lines of code so they become a valid unit test. Assume that each line would be indented correctly.
+
+# Answer:
+# 1. def test_my_func_returns_hello_world():
+# 2.  result = my_func()
+# 3.  assert result == "Hello, World!"
+
+# Intro to Pytest - id: lImr91
+# Rearrange these lines of code so they become a valid unit test. Assume that each line would be indented correctly.
+
+# Answer:
+# 1. def test_zero_division():
+# 2.  with pytest.raises(ZeroDivisionError):
+# 3.    1 / 0

cb2e59657d768753ed9f0aa7d6d9ebde50ae828f
anmedev <aam2ds@virginia.edu>
2023-02-27 19:12:26 -0500

    add solution for exercise

---
+++ intro-to-tests/automated-tests-exercises.py
@@ -0,0 +1,7 @@
+# Testing - id: 96e36e16-4354-449b-b99b-5d9737766507
+# Why are automated tests beneficial?
+
+# Answer:
+# Improves quality of code
+# Catches potential logical errors within functions
+# Improves team collaboration

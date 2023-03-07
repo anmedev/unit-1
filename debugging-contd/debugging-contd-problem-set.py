f8a4aa0918342033b07d60b52bedc0754d46a5fe
anmedev <aam2ds@virginia.edu>
2023-03-07 18:25:41 -0500

    update solution for question four

---
+++ debugging-contd/debugging-contd-problem-set.py
@@ -0,0 +1,12 @@
+# Debugging Continued - id: a7981c89-552e-4587-9133-341aaa4752f3
+# 1. What happens when you run Moira's code?
+# Answer: When the code is run, it raises an error
+
+# 2. What line is the error on?
+# Answer: The error is on line 19 in the "main.py" file
+
+# 3. What type of error is it?
+# Answer: It is a TypeError
+
+# 4. Why is the line throwing an error?
+# Answer: The line is throwing an error because v_pos is an integer and the code is trying to apply the len() function on it. This will not work because you cannot iterate over an integer.

b1934b37a90ba1ad776f4fd7b7721760e902c01f
anmedev <aam2ds@virginia.edu>
2023-03-07 20:11:28 -0500

    add solution for exercise

---
+++ iterating-over-data/intro-to-iteration-exercises.py
@@ -0,0 +1,7 @@
+# Iteration Examples - id: dfc8fa62-40c7-4489-8ab3-f91eb5c67579
+# Iteration is useful in many scenarios. Mark each scenario that utilizes iteration.
+
+# Answer:
+# Looking through items in an online shopping cart, and adding the cost of each item to the total.
+# Looking through a list of sales transactions, and finding the highest grossing sales transaction of the day.
+# Looking through a clean laundry basket, and finding matching pairs!

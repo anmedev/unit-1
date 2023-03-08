b89a220b1d0e2f5763a1defaf0bed3f009065a36
anmedev <aam2ds@virginia.edu>
2023-03-08 18:31:07 -0500

    add questions for first exercise

---
+++ iterating-over-data/iterating-over-dictionaries-exercises.py
@@ -0,0 +1,17 @@
+# Examples - Menu
+# Follow these steps for each example:
+menu = {
+    "appetizer": "Brussels Sprouts",
+    "beverage": "Fancy Lemonade"
+}
+
+for dish, food in menu.items():
+    print(f"The special {dish} for tonight is the {food}.")
+
+# Read through the code and identify:
+# What is the dictionary?
+# What is each key and value?
+# What do we name each key and value?
+# How do we use each key and value in the loop?
+# Predict what will print
+# Run the code and check your prediction

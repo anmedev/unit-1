f1c9587feccdf3c2e3075d252af3a01846b31c6c
anmedev <aam2ds@virginia.edu>
2023-03-08 16:49:35 -0500

    update solution for first exercise and add questiosn for second exercise

--- iterating-over-data/iterating-over-lists-exercises.py
+++ iterating-over-data/iterating-over-lists-exercises.py
@@ -21,4 +21,28 @@ for option in options:
 # What about getting food from the place we always go to tonight?
 # What about getting food from the place that just opened but looks too fancy tonight?
 
+# 3. Run the code and check your prediction -- My prediction is correct.
+
+# Example: Calculating Taxes
+# Follow these steps for each example:
+prices = [18.99, 56.00, 48.50, 18.50]
+
+for price in prices:
+    taxed_price = price * 1.101
+    print(f"The cost of one item is {taxed_price}")
+
+print("That sure was a meal!")
+
+# Read through the code and identify:
+# 1a. What is the list? 
+
+# 1b. What is each element? 
+
+# 1c. What do we name each element? 
+
+# 1d. How do we use each element in the loop? 
+
+# 2. Predict what will print
+
+
 # 3. Run the code and check your prediction

86096de5303da45d2e09bf34b7dc6258adf07ba9
anmedev <aam2ds@virginia.edu>
2023-03-08 16:41:24 -0500

    add solution for first exercise

--- iterating-over-data/iterating-over-lists-exercises.py
+++ iterating-over-data/iterating-over-lists-exercises.py
@@ -8,9 +8,15 @@ for option in options:
     print(f"What about getting food from {option} tonight?")
 
 # Read through the code and identify:
-# What is the list?
-# What is each element?
-# What do we name each element?
-# How do we use each element in the loop?
+# What is the list? The list is called options
+
+# What is each element? Each element represents the dining options a user can choose from. They include: "the place I'm craving but is too far away", "the place we always go to", and "that place that just opened but looks too fancy". All of these list elements are strings.
+
+# What do we name each element? We name each element option
+
+# How do we use each element in the loop? By iterating through each element and storing each element as a value in the option variable inside the print statement.
+
 # Predict what will print
+# What about getting food from the place I'm craving but is too far away tonight?
+
 # Run the code and check your prediction

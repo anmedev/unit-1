6daead35139c418be146206eda8f8382e02f4775
anmedev <aam2ds@virginia.edu>
2023-03-08 12:55:55 -0500

    add file and first question for iterating over lists exercises

---
+++ iterating-over-data/iterating-over-lists-exercises.py
@@ -0,0 +1,16 @@
+# Example: Dining Out
+# Follow these steps for each example:
+options = ["the place I'm craving but is too far away",
+           "the place we always go to",
+           "that place that just opened but looks too fancy"
+           ]
+for option in options:
+    print(f"What about getting food from {option} tonight?")
+
+# Read through the code and identify:
+# What is the list?
+# What is each element?
+# What do we name each element?
+# How do we use each element in the loop?
+# Predict what will print
+# Run the code and check your prediction

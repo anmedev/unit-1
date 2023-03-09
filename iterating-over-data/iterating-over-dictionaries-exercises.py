3e25d7c2814353eab9076f20efe0daaa0aa3a9bd
anmedev <aam2ds@virginia.edu>
2023-03-08 19:35:30 -0500

    add solutions for questions 2 and 3 for first exercise

--- iterating-over-data/iterating-over-dictionaries-exercises.py
+++ iterating-over-data/iterating-over-dictionaries-exercises.py
@@ -22,4 +22,8 @@ for dish, food in menu.items():
 # By iterating over each key and its corresponding value and storing them in the dish and food variables respectively. As the loop runs, it will print the string along with the first key and value then it will print the same string with the second key and value.
 
 # 2. Predict what will print
+# The special appetizer for tonight is Brussel Sprouts.
+# The special beverage for tonight is Fancy Lemonade.
+
 # 3. Run the code and check your prediction
+# My prediction is correct.

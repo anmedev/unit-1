18854479f1ccea44767b753029781b81c664bc0d
anmedev <aam2ds@virginia.edu>
2023-03-09 17:43:03 -0500

    add solutions for remaining exercises

--- iterating-over-data/iterating-over-data-problem-set.py
+++ iterating-over-data/iterating-over-data-problem-set.py
@@ -63,6 +63,50 @@ print(sum_series(3)) # 6
 print(sum_series(5)) # 15
 print(sum_series(10)) # 55
 
+# Iterating Over Data - id: tTFqeV
+# Create a function named sum_series that takes two parameters: min_value and max_value. Starting with min_value, add each number in the range from min_value to max_value together. Then return that total. Required: Use a for-loop and range().
 
+# Answer:
+def sum_series(min_value, max_value):
+  sum = 0
+  for num in range(min_value, max_value + 1):
+    sum += num
+  return sum
+print(sum_series(1, 3)) # 6
+print(sum_series(2, 4)) # 9
+print(sum_series(0, 5)) # 15
+print(sum_series(3, 7)) # 25
+
+# Iterating Over Data - id: 1HUR7V
+# Create a function named sum_even_nums_series that takes two parameters: min_value and max_value. Starting with min_value, add each even number in the range from min_value to max_value together. Then return that total. Required: Use a for-loop.
+
+# Answer:
+def sum_even_nums_series(min_value, max_value):
+  sum = 0
+  for num in range(min_value, max_value + 1):
+    if num % 2 == 0:
+      sum += num
+  return sum
+print(sum_even_nums_series(1, 3)) # 2
+print(sum_even_nums_series(2, 4)) # 6
+print(sum_even_nums_series(0, 5)) # 6
+print(sum_even_nums_series(3, 7)) # 10
+
+# Iterating Over Data - id: NkbCNAWCF
+# Create a function named find_adults. This function has one parameter, people, which is a dictionary. Each key is a string that holds the name of a person.
+# Each value is an integer that is the age of that person. The function should return a list of all the names of people that are 18 or older. The list can be in any order. If there are no adults in the dictionary, the function should return an empty list.
+
+# Answer:
+def find_adults(people):
+  adults_list = []
+  for name, age in people.items():
+    if age >= 18:
+      adults_list.append(name)
+  return adults_list
+
+print(find_adults({"Jane": 23, "Mateo": 2, "Eduardo": 18, "Elsa": 1, "Alba": 66})) # ['Jane', 'Eduardo', 'Alba']
+print(find_adults({"Mateo": 2, "Eduardo": 18, "Elsa": 1})) # ['Eduardo']
+print(find_adults({"Mateo": 2, "Elsa": 1})) # []
+print(find_adults({})) # []
 
 

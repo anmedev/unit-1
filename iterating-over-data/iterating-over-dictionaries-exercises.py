5942a0a4e524d3529dca3795a13a12a8e0732dec
anmedev <aam2ds@virginia.edu>
2023-03-08 19:31:18 -0500

    add solutions for questions 1a through 1d

--- iterating-over-data/iterating-over-dictionaries-exercises.py
+++ iterating-over-data/iterating-over-dictionaries-exercises.py
@@ -9,9 +9,17 @@ for dish, food in menu.items():
     print(f"The special {dish} for tonight is the {food}.")
 
 # Read through the code and identify:
-# What is the dictionary?
-# What is each key and value?
-# What do we name each key and value?
-# How do we use each key and value in the loop?
-# Predict what will print
-# Run the code and check your prediction
+# 1a. What is the dictionary
+# The dictionary is called menu
+
+# 1b. What is each key and value?
+# The first key is "appetizer" and its corresponding value is "Brussel Sprouts". The second key is beverage" and its corresponding value is "Fancy Lemonade". The keys and values represent the menu items available.
+
+# 1c. What do we name each key and value?
+# We name each key dish and we name each value food.
+
+# 1d. How do we use each key and value in the loop?
+# By iterating over each key and its corresponding value and storing them in the dish and food variables respectively. As the loop runs, it will print the string along with the first key and value then it will print the same string with the second key and value.
+
+# 2. Predict what will print
+# 3. Run the code and check your prediction

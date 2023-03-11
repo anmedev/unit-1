7ea504b2b9e0f2edbbd6a82c468912f15c5afb3f
anmedev <aam2ds@virginia.edu>
2023-03-11 09:15:16 -0500

    add solutions for first two problems

---
+++ iterating-over-data/while-loops-problem-set.py
@@ -0,0 +1,18 @@
+# While Loops - id: 31402b5a-5d5a-409f-b5cd-a5c1a7f845d7
+# How many times will the body of this while loop run?
+counter = 0
+while counter < 5:
+    print('in the loop body')
+    counter += 1
+
+# Answer: 5
+
+# While Loops - id: ce90bffc-5966-4486-8c2f-4d1d10ca58d3
+# How many times will the body of this while loop run?
+counter = 4
+while counter >= 0:
+    print('in the loop body')
+    counter -= 1
+
+# Answer: 5
+

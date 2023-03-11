b417cd2b3a495afeefe4baff2b7dade650116e44
anmedev <aam2ds@virginia.edu>
2023-03-10 19:20:28 -0500

    add file and solution for first exercise

---
+++ iterating-over-data/while-loops-exercises.py
@@ -0,0 +1,17 @@
+# While Loops - id: AJC4nw
+# Fix this loop so it loops while i is less than 5 .
+# def loop_until_five():
+#     i = 0
+#     while False:
+#         i += 1
+#     return i
+# result = loop_until_five()
+
+# Answer:
+def loop_until_five():
+    i = 0
+    while i < 5:
+        i += 1
+    return i
+result = loop_until_five()
+print(result)

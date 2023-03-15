da822edf9671d1459349537b9db036c12ab55b96
anmedev <aam2ds@virginia.edu>
2023-03-15 17:04:20 -0400

    add solutions for exercises

---
+++ git-setup/intro-to-git-vocab-and-concepts-exercises.py
@@ -0,0 +1,14 @@
+# Git Vocab I - id: 6ceb3b61-a46f-41a4-b649-5b7a1826b632
+# What is Git?
+
+# Answer: Distributed Version Control System
+
+# Git Vocab II - id: fb75bb2b-258c-403c-a3ba-6f5923777cbe
+# What is an example of a repository?
+
+# Answer: a folder with 1 or more files
+
+# Git Vocab III - id: b0961893-7591-4c5a-9430-0b0c4c0958ae
+# What is a commit?
+
+# Answer: Version(s) of file(s)

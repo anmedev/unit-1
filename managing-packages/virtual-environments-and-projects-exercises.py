ae38254514a818454d08a3947fc474667aad2943
anmedev <aam2ds@virginia.edu>
2023-03-22 19:32:09 -0400

    add solution for exercise

---
+++ managing-packages/virtual-environments-and-projects-exercises.py
@@ -0,0 +1,9 @@
+# Virtual Environments and Projects - id: JGjW8W
+# What is the general correct order of steps for creating, then activating, then installing the dependencies listed in a file, and finally deactivating a virtual environment?
+
+# Answer:
+# Step 1: cd project-root-folder
+# Step 2: python3 -m venv venv
+# Step 3: source venv/bin/activate
+# Step 4: pip install -r requirements.txt
+# Step 5: deactivate

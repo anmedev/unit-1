6233ba8b628abb694f10108a4714417ee4774f35
anmedev <aam2ds@virginia.edu>
2023-04-25 13:32:17 -0400

    add solutions for the first two exercises

---
+++ nested-lists-and-dictionaries/nested-lists-and-dictionaries-exercises.py
@@ -0,0 +1,20 @@
+# Nested Lists and Dictionaries - id: c49169ab-1a7c-447b-9399-4ee331e5d596
+# Use 1-4 sentences to describe this data structure. Describe each layer of the data structure, starting with the outer data structure.
+
+clothes = [
+  ["hat", "beanie", "fedora"],
+  ["tshirt", "hoodie", "cardigan"],
+  ["jeans", "joggers", "shorts"]
+]
+
+# Answer: This data structure is a two-dimensional array, or a list of lists. It has an outer list called clothes and inside this clothes list are three inner lists. These three inner lists all have the same length of 3 elements.
+
+# Nested Lists and Dictionaries - id: e4927780-c756-4b5d-9cbe-acdfa0859d1f
+# Use 1-4 sentences to describe this data structure. Describe each layer of the data structure, starting with the outer data structure.
+sandwich = {
+  "blt": ["bacon", "lettuce", "tomato"],
+  "grilled cheese": ["american cheese", "gruyere"],
+  "pbj": ["grape jelly", "peanut butter"]
+}
+
+# Answer: This data structure is a dictionary of lists. It has an outer dictionary called sandwiches and inside this sandwiches dictionary, are three inner lists. These lists are called "blt", "grilled cheese", and "pbj" respectively and they have varying list lengths.

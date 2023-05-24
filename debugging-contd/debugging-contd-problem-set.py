73bf4e0af225abf64e60c49e542b354b3b3d454a
anmedev <aam2ds@virginia.edu>
2023-05-24 14:05:25 -0400

    add solution for second problem

--- debugging-contd/debugging-contd-problem-set.py
+++ debugging-contd/debugging-contd-problem-set.py
@@ -10,3 +10,9 @@
 
 # 4. Why is the line throwing an error?
 # Answer: The line is throwing an error because v_pos is an integer and the code is trying to apply the len() function on it. This will not work because you cannot iterate over an integer.
+
+# Debugging Continued - id: 8c74e298-f8e7-4635-8990-b8cc5791be43
+# Before Moira can fix her nested loop, she needs to make sure that she understands the data structure that she's looping over. Read through the provided functions and use debugging tools like print statements to investigate the stage variable that she is working with.
+
+# What is stage?
+# Answer: Stage is a list of lists. The lists of lists read as: [[False, True, False], [False, True, False], [False, True, False]]. False represents where the cell dies and True represents where the cell lives.

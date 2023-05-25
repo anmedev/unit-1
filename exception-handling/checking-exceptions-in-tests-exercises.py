28517208d06f02ef4b7247116dbe02e4ece19aeb
anmedev <aam2ds@virginia.edu>
2023-05-25 19:03:14 -0400

    comment out code

--- exception-handling/checking-exceptions-in-tests-exercises.py
+++ exception-handling/checking-exceptions-in-tests-exercises.py
@@ -1,41 +1,41 @@
 # Checking Exceptions in Tests - id: 8HwyKa
 # What is the function call that will raise an exception?
 
-def test_check_is_phone_num_valid():
-    phone_num = 777-888-9999
-    with pytest.raises(ValueError):
-        is_valid = is_phone_num_valid(phone_num)
+# def test_check_is_phone_num_valid():
+#     phone_num = 777-888-9999
+#     with pytest.raises(ValueError):
+#         is_valid = is_phone_num_valid(phone_num)
 
 # Answer: is_phone_num_valid(phone_num)
 
 # Checking Exceptions in Tests - id: wrXI7Z
 # What is the kind of exception we're expecting?
 
-def test_check_is_phone_num_valid():
-    phone_num = 777-888-9999
-    with pytest.raises(ValueError):
-        is_valid = is_phone_num_valid(phone_num)
+# def test_check_is_phone_num_valid():
+#     phone_num = 777-888-9999
+#     with pytest.raises(ValueError):
+#         is_valid = is_phone_num_valid(phone_num)
 
 # Answer: ValueError
 
 # Checking Exceptions in Tests - id: TmFBrt
 # What is the function call that will raise an exception?
 
-def test_add_student_to_roster():
-    roster = {}
-    student = "Cordelia Wang"
-    with pytest.raises(AttributeError):
-        add_student_to_roster(roster, student)
+# def test_add_student_to_roster():
+#     roster = {}
+#     student = "Cordelia Wang"
+#     with pytest.raises(AttributeError):
+#         add_student_to_roster(roster, student)
 
 # Answer: add_student_to_roster(roster, student)
 
 # Checking Exceptions in Tests - id: z4Qv6S
 # What is the kind of exception we're expecting?
 
-def test_add_student_to_roster():
-    roster = {}
-    student = "Cordelia Wang"
-    with pytest.raises(AttributeError):
-        add_student_to_roster(roster, student)
+# def test_add_student_to_roster():
+#     roster = {}
+#     student = "Cordelia Wang"
+#     with pytest.raises(AttributeError):
+#         add_student_to_roster(roster, student)
 
 # Answer: AttributeError

551f181c7e361cbcb326b1b989b781c76220c085
anmedev <aam2ds@virginia.edu>
2023-05-25 14:09:42 -0400

    add solutions for remaining problems

--- debugging-contd/debugging-contd-problem-set.py
+++ debugging-contd/debugging-contd-problem-set.py
@@ -24,4 +24,21 @@
 
 # Debugging Continued - id: 2404607e-9db0-48bd-9222-61a58913ffb4
 # Moira's code produces this output: [SCREENSHOT OF ERROR]. What is the bug that Moira sees now? What is the expected output? What is the code doing?
-# Answer: The bug in the code is causing every cell to evaluate to False, which means there are no living cells. The code is setting every cell in the stage are evaluating to False.
+# Answer: The bug in the code is causing every cell to evaluate to False, which means there are no living cells. The expected output is that the first generation should have three vertical living cells and the second generation should have three horizontal living cells. The code is setting every cell in the stage are evaluating to False.
+
+# Debugging Continued - id: f9f85ba0-3390-46c0-9244-38b1bffa6821
+# Moira wants to get her code working, but before she can do that she needs to understand why her code is doing what it's doing. Take a few minutes and use some debugging techniques to find out why her code is behaving the way it is. List a few of the things you tried and what you discovered.
+
+# Answer: Add print statements inside the for loops to print the stage variable value.
+
+# Debugging Continued - id: 882c916d-55ab-4c53-888b-8b8d68ea8021
+# Why doesn't Moira's one_generation function work? Why are all of the cells dead in the second generation in the test?
+
+# Answer: The code is changing the state of the cells before the neighbors are fully counted in the count_neighbors function.
+
+# Debugging Continued - id: 4b1ba8ea-b9e5-4c37-b486-17fd865bc17b
+# Moira knows what's happening and why it's happening. She's not quite sure yet how to fix her code, though! What are some debugging steps she can take as she works on a solution?
+
+# Answer:
+# Use print statements to get more information
+# Use the Python Debugger

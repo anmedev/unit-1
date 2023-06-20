fdc1c81b965fa069b9d4ab33069cf13d200e70d8
anmedev <aam2ds@virginia.edu>
2023-06-20 14:28:15 -0400

    add Ada's explanation for time complexity

--- cs-fundamentals/big-o/binary-search-exercises.py
+++ cs-fundamentals/big-o/binary-search-exercises.py
@@ -21,3 +21,5 @@ def binary_search(input_list, value):
 print(binary_search([1,2,3,4,5,6], 4))
 
 # Answer: O(log n)
+
+# Ada's Explanation: There is one loop in this method. The number of times the loop runs is proportional to the length of input_list. With each iteration through the loop, half of the items in input_list are eliminated (either the first half or the second half) until either the value is found or the loop ends. Since with each iteration through the loop, half of the values in the remaining sub-array are eliminated, the time complexity of this algorithm is logarithmic or O(log n) where n is the length of the input parameter, input_list.

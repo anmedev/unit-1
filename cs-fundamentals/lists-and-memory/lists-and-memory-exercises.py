579986338dcf93c64ada540448fa116485a8df37
anmedev <aam2ds@virginia.edu>
2023-06-22 15:26:44 -0400

    add solutions for exercise

---
+++ cs-fundamentals/lists-and-memory/lists-and-memory-exercises.py
@@ -0,0 +1,10 @@
+# In Python how are variables stored? - id: 7e08e867-3d1e-4ea4-9465-c6a4f38d0ef0
+# In Python variables store...
+
+# Answer: references to where data is stored
+
+# Big O: List elements by Index - id: a25e5d9f-13d4-438a-a256-21f4ae2f0835
+# What is the Big O of for changing an element by its index number like students[n] = "Ceicil"?
+
+# Answer: O(1) - Constant
+# Ada's Explanation: Since looking up an element by its index just involves some arithmetic and does not depend on the size of the given array, the time/space complexity are both O(1).

397204aef607a212702e4cf40c294a21edd2f12a
anmedev <aam2ds@virginia.edu>
2023-06-28 20:46:46 -0400

    add solution for exercise

---
+++ cs-fundamentals/lists-and-memory/lists-as-references-exercises.py
@@ -0,0 +1,7 @@
+# Why Side Effects Are Problematic - id: 405c0a8c-d9b9-428e-8a72-3cf173bbff4a
+# Why are functions with side effects problematic?
+
+# Answer:
+# They make our functions harder to test
+# They can introduce hard-to-find bugs in our programs
+# Functions with side effects require more communication among team members

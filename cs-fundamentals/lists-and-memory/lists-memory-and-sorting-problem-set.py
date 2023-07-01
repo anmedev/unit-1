b5f44438d6e3ca961ce5e4ea4478a9eb02f682f0
anmedev <aam2ds@virginia.edu>
2023-07-01 15:22:15 -0400

    add solutions for remaining problems

--- cs-fundamentals/lists-and-memory/lists-memory-and-sorting-problem-set.py
+++ cs-fundamentals/lists-and-memory/lists-memory-and-sorting-problem-set.py
@@ -73,3 +73,37 @@ def long_words(words):
 # Answer in 3-6 sentences, draw a picture and describe it, or write a poem, an analogy, or a story.
 
 # Answer: Insertion sort algorithm splits a list into a sorted and unsorted side. Each item in the unsorted side is then put in its proper place on the sorted side of the list. Insertion sort has a quadratic time complexity and a constant space complexity. The factors that contribute to its time complexity include the number of comparisons performed in the inner loop.
+
+# Sorting Algorithms - id: P2sZ7P
+# There's no better way to get familiar with sorting algorithms than to implement one yourself! Without using Python's sort() method (or anything like that), implement sort_by_length based on insertion sort.
+
+# sort_by_length is a function that takes in one string. It returns a list of strings, where the items are the words from the string, ordered by length. Words with shorter length are placed before words with longer length. When words are tied for length, maintain the order they appeared in the original string.
+
+# Ada's Solution:
+def sort_by_length(sentence):
+  array = sentence.split()
+  i = 1
+  while i < len(array):
+    to_insert = array[i]
+    j = i
+        # Search in the sorted portion of the list for the correct position to insert sentence[i]
+    while j > 0 and len(array[j-1]) > len(to_insert):
+      array[j] = array[j-1]
+      j -= 1
+    array[j] = to_insert
+    i += 1
+  return array
+print(sort_by_length("I love great awesome words")) # ["I", "love", "great", "words", "awesome"]
+print(sort_by_length("love great awesome words I")) # ["I", "love", "great", "words", "awesome"]
+print(sort_by_length("")) # []
+print(sort_by_length("words of equal length")) # ["of", "words", "equal", "length"]
+
+# Sorting Algorithms - id: U2QaR9
+# Describe the time complexity of your implementation of sort_by_length.
+
+# Answer: The time complexity is O(n2) - quadratic
+
+# Sorting Algorithms - id: 51774124-db8e-439c-bcc0-f7f03cada84f
+# Describe the space complexity of your implementation of sort_by_length.
+
+# Answer: The space complexity is O(1) - constant

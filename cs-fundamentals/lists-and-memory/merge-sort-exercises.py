e02bb80f71613d3d7619da271ff99ac72bd8ccc4
anmedev <aam2ds@virginia.edu>
2023-07-05 18:12:44 -0400

    add solution for exercise

---
+++ cs-fundamentals/lists-and-memory/merge-sort-exercises.py
@@ -0,0 +1,9 @@
+# Sorting Algorithms - id: oIbAq4
+# Describe the algorithm for Merge Sort in your own words. Make sure to touch on:
+
+# What makes this algorithm unique?
+# What is its time complexity?
+# What factors contribute to its time complexity?
+# Answer in 3-6 sentences, draw a picture and describe it, or write a poem, an analogy, or a story.
+
+# Answer: Merge sort divides an array into two sub-arrays repeatedly into smaller and smaller sub-arrays until the sub-arrays have only one element in it/a size of one. When the sub-arrays have been divided, the sub-arrays will be sorted and merged together into one array. It has a log linear time complexity and the factors that contribute to this time complexity are the division step, merge step, base case, and counting of levels.

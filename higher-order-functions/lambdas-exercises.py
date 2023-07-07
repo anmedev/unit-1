11c79e9b284b147621894f393f5b734ae40c439f
anmedev <aam2ds@virginia.edu>
2023-07-06 21:46:52 -0400

    add solutions for exercises

---
+++ higher-order-functions/lambdas-exercises.py
@@ -0,0 +1,14 @@
+# Higher Order Functions - id: be152c53-2f78-40e1-8c6c-9b3f8b086c17
+# Which of the following are true for lambda expressions?
+
+# Answer: a|b|d
+# a|Lambda expressions are also known as anonymous functions
+# b| Lambda expressions contain only one expression
+# d| Lambda functions can have any number of arguments
+
+# Higher Order Functions - id: c51e0bad-6a27-43d7-acb5-a570c87163d2
+# Which of the following are valid to pass to the key parameter of the min function?
+
+# Answer: c|d
+# c| A variable that points to a function
+# d| A lambda expression

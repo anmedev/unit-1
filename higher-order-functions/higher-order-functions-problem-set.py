bb384030ee37cc1c264a3a86b149173c592e62e6
anmedev <aam2ds@virginia.edu>
2023-07-10 15:45:05 -0400

    add solution for first problem set

---
+++ higher-order-functions/higher-order-functions-problem-set.py
@@ -0,0 +1,9 @@
+# Higher Order Functions - id: 2a948f45-615e-4e79-8f90-260bd231ba28
+# Check all the example function calls that represent valid syntax.
+
+# Answer: a| c| d| e| f|
+# a| function_name(argument1, argument2)
+# c| function_name(parameter1=argument1, parameter2=argument2)
+# d| function_name(argument1, parameter2=argument2)
+# e| function_name(argument1, parameter2=argument2, parameter3=argument3)
+# f| function_name(argument1, argument2, parameter3=argument3)
